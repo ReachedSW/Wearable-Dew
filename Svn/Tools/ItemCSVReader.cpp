4e4023b261998468f4fdac63dd23984895aa9505
Emir <emir210515@ogr.duzce.edu.tr>
2022-03-22 12:24:03 +0300

    Create ItemCSVReader.cpp

---
+++ Svn/Tools/ItemCSVReader.cpp
@@ -0,0 +1,60 @@
+//ara
+
+string arITVType[] =
+
+
+// içine ekle
+
+"ITEM_DEW_S",
+
+
+
+// ara
+
+	string* arIAFVSubType[] =
+
+
+//üstüne ekle
+
+
+	string arIAFVSub44[] = 
+	{
+		"DEW_GREEN",
+		"DEW_BLUE",
+		"DEW_RED",
+		"DEW_ORANGE",
+		"DEW_YELLOW",
+		"DEW_WHITE",
+	};
+
+// ara
+
+	string* arIAFVSubType[] =
+
+
+// içine ekle
+
+arIAFVSub44,
+
+
+// ara
+
+int arNumberOfSubtype[] =
+
+// içine ekle
+
+sizeof(arIAFVSub44) / sizeof(arIAFVSub44[0]),
+
+
+
+// ara
+string arIWFVWearrFlag[] =
+
+// içine ekle
+
+	"WEAR_DEW_GREEN",
+	"WEAR_DEW_BLUE",
+	"WEAR_DEW_RED",
+	"WEAR_DEW_ORANGE",
+	"WEAR_DEW_YELLOW",
+	"WEAR_DEW_WHITE",
